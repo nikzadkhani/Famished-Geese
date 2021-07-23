Added Reward Calculation + Minor Fixes
New reward function -> train.py
Option loading for testing from file -> test.py

DDQN/model.py
@@ -1,6 +1,5 @@
 
 import numpy as np
-
 import torch
 from torch import nn, optim
 from torch.nn import functional as F
@@ -10,11 +9,10 @@ class Net(nn.Module):
 
     def __init__(self, num_states, num_actions):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(num_states, 128)
+        self.fc1 = nn.Linear(num_states, 64)
         self.fc1.weight.data.normal_(0,0.1)
-        self.fc2 = nn.Linear(128,256)
-        self.fc2.weight.data.normal_(0,0.1)
-        self.out = nn.Linear(256,num_actions)
+        self.fc2 = nn.Linear(64,32)
+        self.out = nn.Linear(32,num_actions)
         self.out.weight.data.normal_(0,0.1)
 
     def forward(self,x):
@@ -106,7 +104,7 @@ class DDQN():
       self.optimizer.step()
 
   def ep_decay(self, EPS_DECAY, steps_done):
-      EPS_END = 0.05
+      EPS_END = 0.01
       EPS_START = self.epsilon_start
       self.epsilon = EPS_END + (EPS_START - EPS_END) * (1 - steps_done / EPS_DECAY)
 

DDQN/train.py
@@ -1,14 +1,11 @@
-import kaggle_environments
-import torch
 import os
 from kaggle_environments import make
-from kaggle_environments.envs.hungry_geese.hungry_geese import Observation, Configuration, Action
-import numpy as np
+from kaggle_environments.envs.hungry_geese.hungry_geese import row_col
 
 from model import DDQN
 from vector import create_state_vector, get_action
 from tqdm import trange
-
+import numpy as np
 
 env = make("hungry_geese")
 # Train against greedy
@@ -16,6 +13,50 @@ trainer = env.train([None, "greedy", "greedy", "greedy"])
 trainer.reset()
 
 ACTIONS = ['NORTH', 'SOUTH', 'EAST', 'WEST']
+NUM_COLS = 11
+
+def calculate_reward(state, new_state) -> int:
+  reward = 0
+
+  goose_pos = state.geese[0]
+  new_goose_pos = new_state.geese[0]
+
+  goose_len = len(goose_pos)
+  new_goose_len = len(new_goose_pos)
+
+  # If goose dies, punish corpse
+  if new_goose_len == 0:
+    return -750
+ 
+  # If goose got bigger, give pat on back
+  if new_goose_len > goose_len:
+    return 200
+
+  def manhattan_distance(x1, y1, x2, y2): return abs(x1-x2) + abs(y1-y2)
+
+  # Convert goose heads to cartesian coordinates
+  goose_x, goose_y = row_col(goose_pos[0], NUM_COLS)
+  new_goose_x, new_goose_y = row_col(goose_pos[0], NUM_COLS)
+
+  closest_food = float("inf")
+  for food_pos in state.food:
+    food_x, food_y = row_col(food_pos, NUM_COLS)
+    dist = manhattan_distance(goose_x, goose_y, food_x, food_y)
+    closest_food = min(closest_food, dist)
+
+  new_closest_food = float("inf")
+  for food_pos in new_state.food:
+    food_x, food_y = row_col(food_pos, NUM_COLS)
+    dist = manhattan_distance(new_goose_x, new_goose_y, food_x, food_y)
+    new_closest_food = min(closest_food, dist)
+
+  # if we move closer to food then give a little reward
+  # note we could have just gotten lucky with a food spawn
+  if new_closest_food < closest_food:
+    return 25
+
+  return -5
+  
 
 
 def train_single_episode(ddqn: DDQN):
@@ -30,23 +71,24 @@ def train_single_episode(ddqn: DDQN):
   state = trainer.reset()
   state_vector = create_state_vector(state)
 
-
   while not done or step_counter == 200:
     action_vector = ddqn.choose_action(state_vector)
     action = get_action(action_vector)
 
     # Take our action
-    new_state, reward, done, _ = trainer.step(action)
+    new_state, _, done, _ = trainer.step(action)
     new_state_vector = create_state_vector(new_state)
+
+    reward = calculate_reward(state, new_state)
 
     # Store for experience replay
     ddqn.store_transition(state_vector, action_vector, reward, new_state_vector)
-    total_episode_reward += reward
 
     ddqn.learn()
 
     step_counter += 1
     state_vector = new_state_vector
+    total_episode_reward += reward
   
   return total_episode_reward, len(new_state.geese[0]) != 0
 
@@ -70,7 +112,7 @@ def train(ddqn: DDQN, num_episodes):
       np.savetxt(wins_path, wins, fmt="%1d")
 
   # save final info
-  ddqn_path = os.path.join(ddqn.opt.save_path, 'ddqn-final-' + str(num_episodes).zfill(5))
+  ddqn_path = os.path.join(ddqn.opt.save_path, 'ddqn-final')
   reward_path = os.path.join(ddqn.opt.save_path, 'rewards-final-' + str(num_episodes).zfill(5))
   wins_path = os.path.join(ddqn.opt.save_path, 'wins-final-' + str(num_episodes).zfill(5))
   print("saving final model")